2cc733a01ef8e2575c09f32bf30086f27b48bc06
MVRT <alj.martinez.j@gmail.com>
2025-03-02 12:42:11 +0100

    first commit

---
+++ src/main/java/com/api/crud/controllers/UserController.java
@@ -0,0 +1,37 @@
+package com.api.crud.controllers;
+
+import com.api.crud.models.UserModel;
+import com.api.crud.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ArrayList<UserModel> getUsers(){
+        return this.userService.getUsers();
+    }
+
+    @GetMapping(path="/{id}")
+    public Optional<UserModel> getUserById(@PathVariable Long id){
+        return this.userService.getById(id);
+    }
+
+    @PostMapping
+    public UserModel saveUser(@RequestBody UserModel user){
+        return this.userService.saveUser(user);
+    }
+
+    @PutMapping(path="/{id}")
+    public UserModel updateUserById(@RequestBody UserModel user, @PathVariable Long id){
+        return this.userService.updateById(user, id);
+    }
+}
---
+++ src/main/java/com/api/crud/services/UserService.java
@@ -0,0 +1,39 @@
+package com.api.crud.services;
+
+import com.api.crud.models.UserModel;
+import com.api.crud.repositories.IUserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    IUserRepository userRepository;
+
+    public ArrayList<UserModel> getUsers(){
+        return(ArrayList<UserModel>) userRepository.findAll();
+    }
+
+    public UserModel saveUser(UserModel user){
+        return userRepository.save(user);
+    }
+
+    public Optional<UserModel> getById(Long id){
+        return userRepository.findById(id);
+    }
+
+    public UserModel updateById(UserModel updatedUser, Long id){
+        return userRepository.findById(id).map(user -> {
+            user.setFirstName(updatedUser.getFirstName());
+            user.setLastName(updatedUser.getLastName());
+            user.setEmail(updatedUser.getEmail());
+            return userRepository.save(user);
+        }).orElseThrow(() -> new EntityNotFoundException("Usuario no encontrado"));
+
+    }
+}
